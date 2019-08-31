82f8cd6238ed73743c9d9069f1c4eb305ecd4a63
Felipe Costa <felipevalois_@hotmail.com>
2019-08-30 21:00:50 -0500

    madded some features

--- TextToSpeech/TextToSpeechViewController.swift
+++ TextToSpeech/TextToSpeechViewController.swift
@@ -9,22 +9,63 @@
 import UIKit
 import AVFoundation
 
-class TextToSpeechViewController: UIViewController {
+class TextToSpeechViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
     
     @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var rateSlider: UISlider!
+    @IBOutlet weak var pitchSlider: UISlider!
+    @IBOutlet weak var languagePicker: UIPickerView!
+    
     
     let synthesizer = AVSpeechSynthesizer()
     
+    
+    var arrVoiceLanguages: [String] = []
+    var currentLanguage: String = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        rateSlider.minimumValue = 0.0
+        rateSlider.maximumValue = 1.0
+        pitchSlider.minimumValue = 0.5
+        pitchSlider.maximumValue = 2.0
+        self.languagePicker.delegate = self
+        self.languagePicker.dataSource = self
+        prepareLanguageList()
+
     }
     
     @IBAction func Speak(_ sender: Any) {
         let utterance = AVSpeechUtterance(string: textView.text)
-        utterance.voice = AVSpeechSynthesisVoice(language: "en-GB")
-        utterance.rate = 0.5
-        
+        utterance.voice = AVSpeechSynthesisVoice(language: currentLanguage)
+        utterance.rate = rateSlider.value
+        utterance.pitchMultiplier = pitchSlider.value
         synthesizer.speak(utterance)
     }
+    
+    func prepareLanguageList(){
+        for voice in AVSpeechSynthesisVoice.speechVoices() {
+            let code = (voice as AVSpeechSynthesisVoice).language
+            arrVoiceLanguages.append(code)
+        }
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        print(arrVoiceLanguages.count)
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        print(arrVoiceLanguages.count)
+        return arrVoiceLanguages.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        currentLanguage = arrVoiceLanguages[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return arrVoiceLanguages[row]
+    }
+ 
 }
